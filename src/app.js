b5bb41d5d0b2484bc1e9806b2b77993efa03dad4
Lari <larissaabreux@gmail.com>
2024-06-19 15:39:56 -0300

    post e filtro por tipo

--- src/app.js
+++ src/app.js
@@ -3,38 +3,57 @@ import express, { json } from 'express';
 const app = express();
 app.use(json());
 
+// array que recebe a listagem de items
 const items = [];
 
+// recebe os dados do 'item' inserido pelo usuário para a array 'items'
 app.post("/items", (req, res) => {
-    const item = req.body;
-
-    items.push({
+    const NewItem = req.body;
+    
+    if (!NewItem.name || !NewItem.quantity || !NewItem.type) {
+        res.sendStatus(422).send("As informações não foram enviadas corretamente!");
+        return;
+    }  
+    else if (items.some(item => item.name == NewItem.name)) {
+        res.sendStatus(409).send("Já existe um item com o mesmo nome!");
+        return;
+    } else {
+        items.push({
         id: items.length + 1,
-        ...item
+        ...NewItem
         });
-    res.send("seu item foi adicionado!");
+        res.sendStatus(201).send("Seu item foi adicionado!");
+    }       
 })
 
-// recebe informações
+// recebe todos os items
 app.get('/items', (req, res) => {
-    res.send(items)
+    // app.get('items?type=nome-do-tipo')
+    // filtra a lista por tipos de mercadoria
+    const { type } = req.query;
+
+    if (type) {
+       const filtroType = items.filter(item => {
+            return item.type.includes(type);
+        })
+       return res.send(filtroType);
+    }
+
+    // app.get('/items')
+        res.send(items)
     })
 
-// app.get('/?type=nome-do-tipo', (req, res) => {
-//     const tipos = [
-//         { name: "Laranja" , quantity: 12, type: "fruta" },
-//         { name: "Melância" , quantity: 1, type: "fruta" },
-//         { name: "Uva" , quantity: 3, type: "fruta" },
-//     ]
-//     res.send(tipos)
-//     })
-
+// pega o item pelo Id
 app.get('/items/:id', (req, res) => {
     const id = req.params.id
     const ItemsPorIds = items.find(item => {
         return item.id == Number(id)
     })
-    res.send(ItemsPorIds)
+    if (ItemsPorIds = []) {
+        return sendStatus(404);
+    } else {
+        return res.send(ItemsPorIds);
+    }
     })
 
 
